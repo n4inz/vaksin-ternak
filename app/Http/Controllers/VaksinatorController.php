a3e4c2d2f97d79fe645a9a112b10cc671b45e79b
n4inz <zulkar.n4inz@gmail.com>
2023-06-16 06:17:02 +0800

    install permison

--- app/Http/Controllers/VaksinatorController.php
+++ app/Http/Controllers/VaksinatorController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class VaksinatorController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Vaksinator/User');
+    }
+
+
+    public function addVaksinator()
+    {
+        return Inertia::render('Vaksinator/UserAdd');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LandingPageControllrer;
+use App\Http\Controllers\VaksinatorController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -30,3 +31,9 @@ Route::middleware('tenant')->group(function() {
 Route::get('/dashboard', [HomeController::class , 'index'])->name('dashboard.index');
 
 Route::get('/', [LandingPageControllrer::class , 'index'])->name('landingPage.index');
+
+
+// User Vasinatore
+Route::get('/vaksinator/user', [VaksinatorController::class , 'index'])->name('vaksinator.index');
+Route::get('/vaksinator/user/add', [VaksinatorController::class , 'addVaksinator'])->name('vaksinator.add');
+
