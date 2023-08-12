250b1132aeaa3bcfb045477c329b047cf575333d
n4inz <zulkar.n4inz@gmail.com>
2023-08-12 18:44:45 +0800

    add option pilih

--- resources/js/Pages/landingPage/DaftarVaksin.vue
+++ resources/js/Pages/landingPage/DaftarVaksin.vue
@@ -158,6 +158,8 @@
                         <label for="" class="block text-sm font-medium leading-6 text-gray-900 invisible">Umur</label>
                         <div class="mt-2">
                         <select v-model="form.umur_bulan" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" name="" id="">
+                            <option value="" disabled selected hidden>Pilih</option>
+
                             <option value="Minggu">Minggu</option>
                             <option value="Bulan">Bulan</option>
                              <option value="Tahun">Tahun</option>
@@ -176,6 +178,8 @@
                                 v-model="form.nama_vaksin"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                             >
+                            <option value="" disabled selected hidden>Pilih</option>
+
                                 <option
                                     v-for="item in namaVaksin"
                                     :key="item"
