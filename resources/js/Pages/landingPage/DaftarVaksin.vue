954def4e07670cdea51074cdf998667a554c24b7
n4inz <zulkar.n4inz@gmail.com>
2023-08-12 18:50:09 +0800

    landing pages

--- resources/js/Pages/landingPage/DaftarVaksin.vue
+++ resources/js/Pages/landingPage/DaftarVaksin.vue
@@ -178,7 +178,7 @@
                                 v-model="form.nama_vaksin"
                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                             >
-                            <option value="" disabled selected hidden>Pilih</option>
+                                <option value="" disabled selected hidden>Pilih</option>
 
                                 <option
                                     v-for="item in namaVaksin"
